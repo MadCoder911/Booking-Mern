e2513c2822838ceb852269a7b8289695a55cdfb8
Royce119 <madcoder911@gmail.com>
2023-09-27 22:52:49 +0300

    added another part to the header

--- client/components/header/Header.tsx
+++ client/components/header/Header.tsx
@@ -1,36 +1,41 @@
 import { FaBed, FaPlane, FaCar, FaTaxi } from "react-icons/fa";
+import SearchBar from "./SearchBar";
 const Header = () => {
   return (
-    <div className="bg-[#003580] text-white flex justify-center">
-      <div className="w-[100%] max-w-[1024px] mt-[20px] mb-[100px] ">
+    <div className="bg-[#003580]   relative text-white flex justify-center">
+      <div className=" container w-[100%] mt-[20px] mb-[100px] ">
         <div className="flex gap-[40px] mb-[50px]">
-          <div className="flex items-center gap-[10px] border-[1px] p-[10px] rounded-[20px]">
+          <div className="cursor-pointer flex items-center gap-[10px] hover:scale-[110%] transition-all border-[1px] p-[10px] rounded-[20px]">
             <FaBed />
-            <span>Stays</span>
+            <span className="font-light">Stays</span>
           </div>
-          <div className="flex items-center gap-[10px]">
+          <div className="cursor-pointer flex items-center gap-[10px] hover:scale-[110%] transition-all">
             <FaPlane />
-            <span>Plane</span>
+            <span className="font-light">Flights</span>
           </div>
-          <div className="flex items-center gap-[10px]">
+          <div className="cursor-pointer flex items-center gap-[10px] hover:scale-[110%] transition-all">
             <FaCar />
-            <span>Car Rentals</span>
+            <span className="font-light">Car Rentals</span>
           </div>
-          <div className="flex items-center gap-[10px]">
+          <div className="cursor-pointer flex items-center gap-[10px] hover:scale-[110%] transition-all ">
+            <FaBed />
+            <span className="font-light">Attractions</span>
+          </div>
+          <div className="cursor-pointer flex items-center gap-[10px] hover:scale-[110%] transition-all">
             <FaTaxi />
-            <span>Airport Taxi</span>
+            <span className="font-light">Airport Taxi</span>
           </div>
         </div>
-        <h1 className="headerTitle my-[24px] font-size-[100px] font-semibold">
-          A lifetime of discounts? It's Genius
+        <h1 className="headerTitle my-[24px] font-size-[100px] font-bold text-5xl">
+          Find your next stay
         </h1>
-        <p className="headerDesc my-[24px] font-light">
-          Get rewarded for your travels, unlock instant savings of 10% or more
-          with a free Lamabooking account
+        <p className="headerDesc my-[24px] font-light text-2xl">
+          Search low prices on hotels, homes and much more...
         </p>
-        <button className="headerBtn  bg-[#0071c2] p-[10px] font-light cursor-pointer">
+        <button className="hover:bg-[#1b90e3] hover:scale-[110%] transition-all rounded-md bg-[#0071c2] p-[10px] font-light cursor-pointer">
           Sign in / Register
         </button>
+        <SearchBar />
       </div>
     </div>
   );
---
+++ client/components/header/SearchBar.tsx
@@ -0,0 +1,133 @@
+"use client";
+import { FaBed, FaCalendar } from "react-icons/fa";
+import { BsPerson } from "react-icons/bs";
+import { DateRange } from "react-date-range";
+import { useState } from "react";
+import { format } from "date-fns";
+import "react-date-range/dist/styles.css"; // main style file
+import "react-date-range/dist/theme/default.css"; // theme css file
+const SearchBar = () => {
+  const [openDate, setOpenDate] = useState(true);
+  const [date, setDate] = useState([
+    {
+      startDate: new Date(),
+      endDate: new Date(),
+      key: "selection",
+    },
+  ]);
+  const [openOptions, setOpenOptions] = useState(false);
+  const [options, setOptions] = useState({
+    adult: 1,
+    children: 0,
+    room: 1,
+  });
+  const handleOption = (target: string, operation: string) => {
+    const value = options[target];
+    let newVal = operation === "i" ? value + 1 : value - 1;
+
+    setOptions({ ...options, [target]: newVal });
+    console.log(options);
+  };
+  return (
+    <div className="py-[20px] container w-[100%] h-[30px] bg-white border-[3px] border-solid border-[#febb02] flex items-center justify-around rounded-sm absolute bottom-[-25px]">
+      <div className="headerSearchiTEM flex items-center gap-[10px]">
+        <FaBed className="text-gray-300 text-[20px]" />
+        <input
+          type="text"
+          placeholder="Where are you going?"
+          className="headerSearchInput outline-none"
+        />
+      </div>
+      <div
+        className="relative flex items-center gap-[10px] cursor-pointer"
+        onClick={() => setOpenDate(!openDate)}
+      >
+        <FaCalendar className="text-gray-300" />
+        <span className="headersearchText text-gray-300 cursor-pointer">
+          {`${format(date[0].startDate, "MM/dd/yyyy")} to  ${format(
+            date[0].endDate,
+            "MM/dd/yyyy"
+          )}`}
+        </span>
+
+        <DateRange
+          editableDateInputs={true}
+          onChange={(item) => setDate([item.selection])}
+          moveRangeOnFirstSelection={false}
+          ranges={date}
+          className={`absolute top-[32px] ${
+            !openDate ? "opacity-0" : "opacity-1"
+          } transition-all`}
+        />
+      </div>
+      <div className="headerSearchiTEM flex items-center gap-[10px]">
+        <BsPerson className="text-gray-300 text-[20px]" />
+        <span className="headersearchText text-gray-300 cursor-pointer">
+          {`${options.adult} adult, ${options.children} children, ${options.room} room`}
+        </span>
+        <div className="options absolute top-[50px] bg-white text-gray-300 rounded-md shadow-lg">
+          <div className="optionsItem w-[200px] flex justify-between m-[10px]">
+            <span className="optionText">Adult</span>
+            <div className="optionCounter flex items-center gap-[10px] text-black">
+              <button
+                className="optionCounterBtn w-[30px] border-[1px] border-[#0071c2] rounded-md cursor-pointer"
+                onClick={() => handleOption("adult", "d")}
+              >
+                -
+              </button>
+              <span className="optionsCounterNumber">{options.adult}</span>
+              <button
+                className="optionCounterBtn w-[30px] border-[1px] border-[#0071c2] rounded-md cursor-pointer"
+                onClick={() => handleOption("adult", "i")}
+              >
+                +
+              </button>
+            </div>
+          </div>
+          <div className="optionsItem w-[200px] flex justify-between m-[10px]">
+            <span className="optionText">Children</span>
+            <div className="optionCounter flex items-center gap-[10px] text-black">
+              <button
+                className="optionCounterBtn w-[30px] border-[1px] border-[#0071c2] rounded-md cursor-pointer"
+                onClick={() => handleOption("children", "d")}
+              >
+                -
+              </button>
+              <span className="optionsCounterNumber">{options.children}</span>
+              <button
+                className="optionCounterBtn w-[30px] border-[1px] border-[#0071c2] rounded-md cursor-pointer"
+                onClick={() => handleOption("children", "i")}
+              >
+                +
+              </button>
+            </div>
+          </div>
+          <div className="optionsItem w-[200px] flex justify-between m-[10px]">
+            <span className="optionText">Room</span>
+            <div className="optionCounter flex items-center gap-[10px] text-black">
+              <button
+                className="optionCounterBtn w-[30px] border-[1px] border-[#0071c2] rounded-md cursor-pointer"
+                onClick={() => handleOption("room", "d")}
+              >
+                -
+              </button>
+              <span className="optionsCounterNumber">{options.room}</span>
+              <button
+                className="optionCounterBtn w-[30px] border-[1px] border-[#0071c2] rounded-md cursor-pointer"
+                onClick={() => handleOption("room", "i")}
+              >
+                +
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="headerSearchiTEM flex items-center gap-[10px]">
+        <button className="hover:bg-[#1b90e3] hover:scale-[110%] transition-all rounded-md bg-[#0071c2] px-[10px] py-[5px] font-light cursor-pointer">
+          Search
+        </button>
+      </div>
+    </div>
+  );
+};
+export default SearchBar;
